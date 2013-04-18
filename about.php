a4b34226881209e6e1555220ac371ddad0f3428b
Deolu Akande <akandeadeoluwa@gmail.com>
2013-04-18 10:24:51 +0100

    removed unnecessary images to make the site faster

--- about.php
+++ about.php
@@ -1,61 +1,45 @@
 <?php
 include 'header.php';
 ?>
-		<div class="main-box">
-			<div class="container">
-				<div class="inside">
-					<div class="wrapper">
-						<!-- aside -->
-						<aside>
-							<!--h2>The <span>Company</span></h2-->
-							<div class="img-box">
-								<figure><img src="images2/Final-Official-Logo (1).jpg" width="150px" height="100px" alt="">
-								</figure>
-								<br/>
-								<!--iQube is a stealth R&amp;D IT / Telecoms company booming out of Nigeria. Aimed at creating world-class solutions to solve local problems. Redefining the art of creativity.Given that there is a large need for sound technical skills in order to create a world class solution, we saw the need to create an avenue for us to acquire and share knowledge hence <strong>Tech Training eXpo (TTX)</strong> 
-							</div>
-							<h2>Our <span>Mission/ Vision</span></h2>
-							<p>
-								Its a young company under construction being driven by some of the best young minds
-							</p>
-							<h2>Our<span> Values</span></h2>
-							<ol>
-								<li>
-									Be positive
-								</li>
-								<li>
-									Trust is free, but expensive
-								</li>
-								<li>
-									Saying <strong>&ldquo; Thank you&rdquo;</strong> is not too expensive
-								</li>
-							</ol>
-							<p>
-								&nbsp;
-							</p-->
-						</aside>
-						<!-- content -->
-						<section id="content">
-							<article>
-								<h2>The <span>Company</span></h2>
-								<!-- .team-list -->
-								<p class="p2">
-									iQube is an Applied Computer Science Research &amp; Development Company that aims to provide solutions to challenges that are locally relevant using state-of-the-art technologies.
-</p><p>iQube is made up of a team of multi-talented researchers, web and mobile application developers, graphics designers, network
-engineers and more. We love solving challenging software problems that require the application of fundamental principles in Computer
-science and Mathematics statistics. We provide custom software solutions to companies and individuals based on their needs, tailored to
-make day-to-day work easier.</p><p>
-Asides solving local problems using technology tools, iQube has been in the business of human resources trainings for companies and
-career development for individuals in the information `technology industry and related fields. iQube has team of coaches and tutors that train
-individuals in a well structured environment from anywhere in the world. This is due to a philosophy that iQube believes in that given the
-right skill set and environment, a willing individual can accomplish great results.
-								</p>
-							</article>
-						</section>
-					</div>
-				</div>
+<div class="main-box">
+	<div class="container">
+		<div class="inside">
+			<div class="wrapper">
+				<!-- aside -->
+				<aside>
+					<!--h2>The <span>Company</span></h2-->
+					<div class="img-box">
+						<figure><img src="images2/Final-Official-Logo (1).jpg" width="150px" height="100px" alt="">
+						</figure>
+						<br/>
+						
+				</aside>
+				<!-- content -->
+				<section id="content">
+					<article>
+						<h2>The <span>Company</span></h2>
+						<!-- .team-list -->
+						<p>
+							iQube is an Applied Computer Science Research &amp; Development Company that aims to provide solutions to challenges that are locally relevant using state-of-the-art technologies.
+						</p>
+						<p>
+							iQube is made up of a team of multi-talented researchers, web and mobile application developers, graphics designers, network
+							engineers and more. We love solving challenging software problems that require the application of fundamental principles in Computer
+							science and Mathematics statistics. We provide custom software solutions to companies and individuals based on their needs, tailored to
+							make day-to-day work easier.
+						</p>
+						<p>
+							Asides solving local problems using technology tools, iQube has been in the business of human resources trainings for companies and
+							career development for individuals in the information technology industry and related fields. iQube has team of coaches and tutors that train
+							individuals in a well structured environment from anywhere in the world. This is due to a philosophy that iQube believes in that given the
+							right skill set and environment, a willing individual can accomplish great results.
+						</p>
+					</article>
+				</section>
 			</div>
 		</div>
-		<?php
-		include 'footer.php';
-		?>
+	</div>
+</div>
+<?php
+include 'footer.php';
+?>
